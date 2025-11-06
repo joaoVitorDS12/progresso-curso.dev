1fca5cb21df18231c43b96265b6f399eee069312
Joao Vitor dos Santos <jvitorgit@gmail.com>
2025-11-05 21:55:46 -0300

    adds `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,32 +1,37 @@
-import { Pool } from "pg";
-
-const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  ssl: getSSLValues(),
-});
+import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = await pool.connect();
-
+  let client;
   try {
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.error("Database query error", error);
-    throw error;
+    console.error(error);
   } finally {
-    client.release();
+    client.end();
   }
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
 
 function getSSLValues() {
-  return process.env.NODE_ENV === "development" ? false : true;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
