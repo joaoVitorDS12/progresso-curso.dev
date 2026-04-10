e33322d3598677a5e7b942fe6da4bdc43e73f2c6
Joao Vitor dos Santos <jvitorgit@gmail.com>
2026-04-10 20:34:45 -0300

    fix: correct email on `activation` and correct link on `webserver`

--- infra/webserver.js
+++ infra/webserver.js
@@ -7,7 +7,7 @@ function getOrigin() {
     return `https://${process.env.VERCEL_URL}`;
   }
 
-  return "https://progresso-curso-dev.vercel.app/";
+  return "https://codebyjoaovitor.com.br/";
 }
 
 const webserver = {
