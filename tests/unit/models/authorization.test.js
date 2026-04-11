34fa4a77536aaf03e9775bb1f80288411ff67eb9
Joao Vitor dos Santos <jvitorgit@gmail.com>
2026-04-11 20:13:23 -0300

    test: fix ISO date format in `authorization` unit tests

--- tests/unit/models/authorization.test.js
+++ tests/unit/models/authorization.test.js
@@ -84,8 +84,8 @@ describe("models/authorizathion.js", () => {
         id: 1,
         username: "Resource",
         features: ["read:user"],
-        created_at: "2026-0101T00:00:00.000Z",
-        updated_at: "2026-0101T00:00:00.000Z",
+        created_at: "2026-01-01T00:00:00.000Z",
+        updated_at: "2026-01-01T00:00:00.000Z",
         email: "resource@resource.com",
         password: "resource",
       };
@@ -100,8 +100,8 @@ describe("models/authorizathion.js", () => {
         id: 1,
         username: "Resource",
         features: ["read:user"],
-        created_at: "2026-0101T00:00:00.000Z",
-        updated_at: "2026-0101T00:00:00.000Z",
+        created_at: "2026-01-01T00:00:00.000Z",
+        updated_at: "2026-01-01T00:00:00.000Z",
       });
     });
   });
